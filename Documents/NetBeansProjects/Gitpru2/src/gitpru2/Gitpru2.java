574648cbf4b39b7bd3632b58a818c778e49bd164
jmgh85 <jmgh85@gmail.com>
2021-03-25 10:20:27 +0100

    xx

--- Documents/NetBeansProjects/Gitpru2/src/gitpru2/Gitpru2.java
+++ Documents/NetBeansProjects/Gitpru2/src/gitpru2/Gitpru2.java
@@ -15,7 +15,7 @@ public class Gitpru2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-         String exampleString = "This is just a sample string";
+         String exampleString = "This is just a sample string2";
 
         int stringLength = exampleString.length();
 
